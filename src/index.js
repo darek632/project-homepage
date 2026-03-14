99b4ab7380977063780ee016c6cdc5108d950c1f
“darek632” <162553961+darek632@users.noreply.github.com>
2026-03-14 13:50:39 +0000

    initial commit, create working page on desktop

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let main = document.querySelector("main");
+let newText = document.createElement("p");
+
+newText.textContent = "what's up mofos, wagwan my slime";
+newText.classList.add("roboto-main-text");
+
+// main.appendChild(newText);
